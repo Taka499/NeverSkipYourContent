3d4980e21b5013625b9b3afa2703e8fb9b969b49
Taka499 <takatomo.saito.27@gmail.com>
2025-08-12 21:48:05 +0900

    old code clean up

--- client/ai-agent/core/agent/llm.py
+++ client/ai-agent/core/agent/llm.py
@@ -1,17 +1,17 @@
 # core/agent/llm.py
 
-from langchain_openai import OpenAI
+from langchain_openai import ChatOpenAI
 
 from core.config import get_settings
 
 _model = None
 
 
-def get_llm() -> OpenAI:
+def get_llm() -> ChatOpenAI:
     global _model
     llm_settings = get_settings()
     if _model is None:
-        _model = OpenAI(
+        _model = ChatOpenAI(
             api_key=llm_settings.OPENAI_API_KEY,
             model=llm_settings.OPENAI_MODEL,
         )
