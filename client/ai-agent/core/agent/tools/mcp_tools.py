e7dba61ee3660248ded5cc3093df3dfe2811a386
Taka499 <takatomo.saito.27@gmail.com>
2025-08-13 23:13:24 +0900

    added two mcp servers' config

--- client/ai-agent/core/agent/tools/mcp_tools.py
+++ client/ai-agent/core/agent/tools/mcp_tools.py
@@ -1,8 +1,33 @@
 # core/agent/tools/mcp_tools.py
 
 from langchain_mcp_adapters.client import MultiServerMCPClient
+from pathlib import Path
 
-mcp_config = {} # TODO: Define the MCP configuration here / or fetch from settings
+
+mcp_config = {
+        "web_search": {
+            "command": "uv",
+            "args": [
+                "--directory",
+                str(Path(__file__).parent / "../../../../../servers/web_search/"),
+                "run",
+                "python",
+                "run_server.py"
+            ],
+            "transport": "stdio",
+        },
+        "page_analyzer": {
+            "command": "uv",
+            "args": [
+                "--directory",
+                str(Path(__file__).parent / "../../../../../servers/page_analyzer/"),
+                "run",
+                "python",
+                "run_server.py"
+            ],
+            "transport": "stdio",
+        }
+    } # TODO: Define the MCP configuration here / or fetch from settings
 
 
 async def get_mcp_tools():
